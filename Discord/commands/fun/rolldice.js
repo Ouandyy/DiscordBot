64e524eb2af3624d09fa81345233acce2ec9b2ce
Wesley Iem <wesleyi@zillowgroup.com>
2019-08-09 11:14:23 -0700

    Fixed roll dice to fit commando format

--- Discord/commands/fun/rolldice.js
+++ Discord/commands/fun/rolldice.js
@@ -1,6 +1,22 @@
-const rolldice = () => {
-    var words = [1,2,3,4,5,6];
-    return words[Math.floor(Math.random() * words.length)]
-}
+const Commando = require('discord.js-commando');
 
-module.exports = rolldice
+module.exports = class rollDice extends Commando.Command{
+    constructor(client) {
+        super(client, {
+            name: 'rolldice',
+            group: 'fun',
+            memberName: 'rolldice',
+            format: 'rolldice',
+            clientPermissions: ['SEND_MESSAGES','CONNECT','SPEAK'],
+            description: 'Command to roll dice'
+        });
+    };  
+
+    run(msg) {
+        const rolldice = () => {
+            var words = [1,2,3,4,5,6];
+            return words[Math.floor(Math.random() * words.length)]
+        }
+        return msg.say(rolldice())
+    };
+};
